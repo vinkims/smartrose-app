d8a42025358bc0e3e7db87326b61e3428dbfc178
Vincent Kimutai <vincent@aspira.co.ke>
2021-03-08 08:34:12 +0300

    feat: Create a dropdown component

---
+++ src/components/DropDown.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
+
+const {width, height} = Dimensions.get('screen')
+
+export default function DropDown({dropDownItems, placeholder, ...rest}){
+    return(
+        <DropDownPicker
+            items = {dropDownItems}
+            placeholder = {placeholder}
+            customArrowDown = {() => <Icon name = "arrow-down" size = {18} />}
+            customArrowUp = {() => <Icon name = "arrow-up" size = {18} />}
+            containerStyle = {{height: 60, width: width / 1.3, paddingTop: 10}}
+            itemStyle = {{justifyContent: 'flex-start'}}
+            labelStyle = {{fontSize: 15}}
+            {...rest}
+        />
+    );
+}
