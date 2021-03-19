80501dbed5b079e07b89b8b513a10db5390f1f66
Vincent Kimutai <vincent@aspira.co.ke>
2021-03-19 11:20:46 +0300

    feat: Create date formatting util

---
+++ src/utils/FormattingUtil.js
@@ -0,0 +1,21 @@
+module.exports = {
+    formatDate(dateString){
+        if (!dateString || dateString.length === 0){
+            return ''
+        }
+
+        // Get parameters to create date object
+        const year = dateString.slice(0, 4)
+        const month = dateString.slice(5, 7)
+        const day = dateString.slice(8, 10)
+        /*const hour = dateString.slice(11, 13)
+        const minute = dateString.slice(14, 16)
+        const second = dateString.slice(17, 19)*/
+
+        // Make an object with the data
+        //const dateObj = new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`)
+        const dateObject = `${day}-${month}-${year}`
+        
+        return dateObject       
+    }
+}
