8aa8871d21f8751e58011e3cb2f62e7617114cd7
Vincent Kimutai <vincent@aspira.co.ke>
2021-07-14 16:38:22 +0300

    Add server communication util

--- src/utils/ServerCommunication.js
+++ src/utils/ServerCommunication.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function firebaseErrors(err){
     if (err.code === "auth/email-already-in-use"){
@@ -20,6 +21,128 @@ function firebaseErrors(err){
     }
 }
 
+function basicResponseHandler(url, resp){
+    
+    console.log('Response', resp);
+    if (resp.ok){
+        return resp.json();
+    } else if (resp.status === 400){
+        return resp.json()
+        .then( jsonResp =>{
+            return {validationError: jsonResp}
+        });
+    } else{
+        if (resp.status === 401 || resp.status === 403){
+            console.log(`Auth Failed for URL, ${url}`);
+        }
+        throw Error(`Failed to call URL: ${url}, \nStatus code: ${resp.status}`)
+    }
+}
+
+
+function get(url){
+    return new Promise((resolve, reject) =>{
+        AsyncStorage.getItem("token")
+        .then(token =>{
+            fetch(url, {
+                headers:{
+                    Accept: 'application/json',
+                    'Content-Type' : 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then( resp => basicResponseHandler(url, resp))
+            .then(respJson =>{
+                console.log('Successfully retrieved from URL ', respJson);
+                resolve(respJson)
+            })
+            .catch(error =>{
+                console.log('Error retrieving from url ', error)
+                reject(error)
+            })
+        })
+    })
+}
+
+
+function post(url, payload){
+    return new Promise(async(resolve, reject) =>{
+        AsyncStorage.getItem("token")
+        .then(token =>{
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type' : 'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(payload)
+            })
+            .then(resp => basicResponseHandler(url, resp))
+            .then(respJson => {
+                resolve(respJson)
+            })
+            .catch(error =>{
+                console.log('Error posting to url ', error)
+                reject(error)
+            })
+        })
+    })
+}
+
+
+function postNoAuth(url, payload){
+    return new Promise(async(resolve, reject) =>{
+        try{
+            let request = {
+                method: 'POST',
+                headers:{
+                    Accept: 'application/json',
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(payload)
+            }
+
+            const resp = await fetch(url, request);
+            const json = basicResponseHandler(url, resp)
+            resolve(json)
+        } catch(error){
+            console.log('Error posting to url ', error);
+            reject(error)
+        }
+    })
+}
+
+
+function patch(url, payload){
+    return new Promise(async(resolve, reject) =>{
+        AsyncStorage.getItem("token")
+        .then(token =>{
+            fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type' : 'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(payload)
+            })
+            .then(resp => basicResponseHandler(url, resp))
+            .then(respJson =>{
+                resolve(respJson)
+            })
+            .catch(error =>{
+                console.log('Error posting to url ', error)
+                reject(error)
+            })
+        })
+    })
+}
+
 export default{
-    firebaseErrors
+    firebaseErrors,
+    post,
+    postNoAuth,
+    patch,
+    get
 }
