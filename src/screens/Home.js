ee8bd4d5488a5da83f6840f4275d8c98f7bc5ea8
vinkims <vincekigen@gmail.com>
2022-05-19 17:03:14 +0300

    feat(Report)- Add weekly data line charts

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Dimensions, ScrollView, StyleSheet, Text, ToastAndroid, View} from 'react-native';
 import jwt_decode from 'jwt-decode';
-import { LineChart } from 'react-native-chart-kit';
+import { LineChart, StackedBarChart } from 'react-native-chart-kit';
 
 import { colors } from '../config/values';
 import ActionButton from '../components/ActionButton';
@@ -23,10 +23,15 @@ export default function HomeScreen({navigation}){
   const [currentWeekSaleData, setCurrentWeekSaleData] = useState([]);
   const [currentLabelDays, setCurrentLabelDays] = useState(["M", "T", "W", "T", "F", "S", "S"]);
   const [currentWeekSale, setCurrentWeekSale] = useState([0,0,0,0,0,0,0]);
+  const [gentsWeeklyTotal, setGentsWeeklyTotal] = useState([0,0,0,0]);
+  const [kidsWeeklyTotal, setKidsWeeklyTotal] = useState([0,0,0,0]);
+  const [ladiesWeeklyTotal, setLadiessWeeklyTotal] = useState([0,0,0,0]);
   const [loading, setLoading] = useState(false);
   const [previousLabelDays, setPreviousLabelDays] = useState(["M", "T", "W", "T", "F", "S", "S"]);
   const [previousWeekSale, setPreviousWeekSale] = useState([0,0,0,0,0,0,0]);
   const [user, setUser] = useState('');
+  const [weeklyData, setWeeklyData] = useState([]);
+  const [weeklyLabels, setWeeklyLabels] = useState(["Week1", "Week2", "Week3", "Week4"]);
 
   useEffect(() =>{
     checkTokenValidity();
@@ -58,6 +63,49 @@ export default function HomeScreen({navigation}){
       }
     ]
   }
+
+  const lineChartConfig = {
+    backgroundGradientFrom: '#fff',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#fff',
+    backgroundGradientToOpacity: 0.5,
+    fillShadowGradientOpacity: 0,
+    color: (opacity = 1) => '#023047',
+    labelColor: (opacity = 1) => '#333',
+    strokeWidth: 2,
+    useShadowColorFromDataset: false,
+    decimalPlaces: 0
+  }
+
+  const lineChartData = {
+    labels: weeklyLabels,
+    datasets: [
+      {
+        data: gentsWeeklyTotal
+      },
+      {
+        data: kidsWeeklyTotal
+      },
+      {
+        data: ladiesWeeklyTotal
+      }
+    ]
+  }
+
+  const lineChartLegends = [
+    {
+      name: 'Gents',
+      color: '#003049'
+    },
+    {
+      name: 'Kids',
+      color: '#d62828'
+    },
+    {
+      name: 'Ladies',
+      color: '#f77f00'
+    }
+  ]
 
   const previousWeekData = {
     labels: previousLabelDays,
@@ -108,6 +156,9 @@ export default function HomeScreen({navigation}){
             previousWeekSale.push(item.totalSalePrice);
           });
           setCurrentWeekSaleData(resp.content.currentWeekSale);
+          getWeeklyGentsData(resp.content.gentsWeeklyTotal);
+          getWeeklyKidsData(resp.content.kidsWeeklyTotal);
+          getWeeklyLadiesData(resp.content.ladiesWeeklyTotal);
         }
       })
       .catch(error => {
@@ -115,6 +166,34 @@ export default function HomeScreen({navigation}){
         LoggerUtil.logError("Home.getReport", error);
         ToastAndroid.show("Error getting report", ToastAndroid.LONG);
       })
+  }
+
+  const getWeeklyGentsData = (data) => {
+    var gentsData = data;
+    var dataSize = Object.keys(data).length;
+    gentsWeeklyTotal.splice(0, dataSize - 1);
+    gentsWeeklyTotal.push(gentsData.weekOne === null ? 0 : gentsData.weekOne);
+    gentsWeeklyTotal.push(gentsData.weekTwo === null ? 0 : gentsData.weekTwo);
+    gentsWeeklyTotal.push(gentsData.weekThree === null ? 0 : gentsData.weekThree);
+    gentsWeeklyTotal.push(gentsData.weekFour === null ? 0 : gentsData.weekFour);
+  }
+
+  const getWeeklyKidsData = (data) => {
+    var dataSize = Object.keys(data).length;
+    kidsWeeklyTotal.splice(0, dataSize - 1);
+    kidsWeeklyTotal.push(data.weekOne === null ? 0 : data.weekOne);
+    kidsWeeklyTotal.push(data.weekTwo === null ? 0 : data.weekTwo);
+    kidsWeeklyTotal.push(data.weekThree === null ? 0 : data.weekThree);
+    kidsWeeklyTotal.push(data.weekFour === null ? 0 : data.weekFour);
+  }
+
+  const getWeeklyLadiesData = (data) => {
+    var dataSize = Object.keys(data).length;
+    ladiesWeeklyTotal.splice(0, dataSize - 1);
+    ladiesWeeklyTotal.push(data.weekOne === null ? 0 : data.weekOne);
+    ladiesWeeklyTotal.push(data.weekTwo === null ? 0 : data.weekTwo);
+    ladiesWeeklyTotal.push(data.weekThree === null ? 0 : data.weekThree);
+    ladiesWeeklyTotal.push(data.weekFour === null ? 0 : data.weekFour);
   }
 
   const logout = async() =>{
@@ -204,6 +283,16 @@ export default function HomeScreen({navigation}){
       <View style = {globalStyles.container}>
         <View style = {styles.saleView}>
             { renderWeeklyData() }
+        </View>
+        <View>
+          <LineChart
+            data = {lineChartData}
+            fillShadowGradient = "#ccc"
+            width = {width / 1.05}
+            height = {270}
+            legend = {lineChartLegends}
+            chartConfig = {lineChartConfig}
+          />
         </View>
       </View>
     </ScrollView>
